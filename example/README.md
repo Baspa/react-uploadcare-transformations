fb22c63919797baf436a3175d94a302eb2ffa773
Bas van Dinther <bas@vormkracht10.nl>
2022-09-23 11:11:39 +0200

    Convert to Typescript (wip)

--- example/README.md
+++ example/README.md
@@ -3,3 +3,9 @@ This example was bootstrapped with [Create React App](https://github.com/faceboo
 It is linked to the react-uploadcare-transformations package in the parent directory for development purposes.
 
 You can run `npm install` and then `npm start` to test your package.
+
+## Development 
+If you get the following error: `digital envelope routines::unsupported` when running `npm install` or `npm run build`, use this command:
+```bash
+export NODE_OPTIONS=--openssl-legacy-provider
+```
---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import styles from './styles.module.css'
+
+interface Props {
+  text: string
+}
+
+export const ExampleComponent = ({ text }: Props) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
+
+// import React, { useEffect } from 'react'
+// import { transformationFinder } from './transformations'
+
+// export const UCImage = ({
+//   uuid,
+//   cdn = 'https://ucarecdn.com/',
+//   filename,
+//   classname,
+//   ...props
+// }) => {
+//   const [url, setUrl] = React.useState('')
+
+//   // Set base URL on initiazation
+//   useEffect(() => {
+//     setUrl(`${cdn}${uuid}`)
+//   }, [uuid, cdn])
+
+//   /**
+//    * Generate URL with transformations
+//    *
+//    * @returns {string} - The final URL with all transformations applied
+//    */
+//   const generateUrl = () => {
+//     let ucUrl = url
+
+//     // Loop through all props and find the transformation
+//     for (const [key, value] of Object.entries(props)) {
+//       // Add transformation to URL
+//       ucUrl = transformationFinder(key)(ucUrl, value)
+//     }
+
+//     // If filename is provided, add it to the URL
+//     if (filename) {
+//       return `${ucUrl}/${filename}`
+//     }
+
+//     return `${ucUrl}/`
+//   }
+
+//   return <img src={generateUrl()} className={classname} />
+// }
